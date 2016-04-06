5d48f01a0eef4fe7ba55bab477202fc479504451
peskovsb <peskov.s.b@gmail.com>
2016-04-06 12:10:18 +0300

    new3

---
+++ migrations/m160405_083602_add_user_role_field.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160405_083602_add_user_role_field extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('{{%user}}', 'role', $this->string(64));
+ 
+        $this->update('{{%user}}', ['role' => 'user']);
+    }
+ 
+    public function down()
+    {
+        $this->dropColumn('{{%user}}', 'role');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
